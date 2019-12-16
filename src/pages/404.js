21e90dd8da81b3b3d995d390ff5bf143919ef3be
Agastya Chandrakant <me@hanabi.in>
2019-12-16 20:15:49 +0530

    change 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,23 @@ function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>Article not found.</h1>
+      <h4>It is okay.</h4>
+      <blockquote>
+        <p>
+          [The roads] moved with the land, you know? It rose, it fell, it
+          curved.
+          <br />
+          Cars didn't drive on it to make great time.
+          <br />
+          They drove on it to have a great time.
+        </p>
+        <p>
+          <cite>
+            &mdash; Sally, <em>Cars (2006)</em>
+          </cite>
+        </p>
+      </blockquote>
     </Layout>
   );
 }
