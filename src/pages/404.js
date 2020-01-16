8ecc5e6fa05e53da1702cc83d0869aeb84a6042d
Agastya Chandrakant <me@hanabi.in>
2020-01-17 03:52:22 +0530

    Fixing 404 and about

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,22 +7,26 @@ function NotFoundPage() {
   return (
     <Layout heading="404: Not found">
       <SEO title="404: Not found" />
-      <h1>It is okay, it is all right.</h1>
-      <blockquote>
-        <p>
-          [The roads] moved with the land, you know? It rose, it fell, it
-          curved.
-          <br />
-          Cars didn't drive on it to make great time.
-          <br />
-          They drove on it to have a great time.
-        </p>
-        <p>
-          <cite>
-            &mdash; Sally Carrera, <em>Cars (2006)</em>
-          </cite>
-        </p>
-      </blockquote>
+      <article class="entry" lang="en">
+        <div className="entry-content">
+          <h1>It is okay, it is all right.</h1>
+          <blockquote>
+            <p>
+              [The roads] moved with the land, you know? It rose, it fell, it
+              curved.
+              <br />
+              Cars didn't drive on it to make great time.
+              <br />
+              They drove on it to have a great time.
+            </p>
+            <p>
+              <cite>
+                &mdash; Sally Carrera, <em>Cars (2006)</em>
+              </cite>
+            </p>
+          </blockquote>
+        </div>
+      </article>
     </Layout>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,15 +6,17 @@ function About() {
   return (
     <Layout heading="About" slug="about">
       <SEO title="About" />
-      <div className="entry-content">
-        <p>
-          Hi, I’m Agastya. Netizens know me as <em>acagastya</em>.
-        </p>
-        <p>
-          I am an undergrad student, and I aim to write about various
-          mathematical and computational topics on this website.
-        </p>
-      </div>
+      <article class="entry" lang="en">
+        <div className="entry-content">
+          <p>
+            Hi, I’m Agastya. Netizens know me as <em>acagastya</em>.
+          </p>
+          <p>
+            I am an undergrad student, and I aim to write about various
+            mathematical and computational topics here.
+          </p>
+        </div>
+      </article>
     </Layout>
   );
 }
