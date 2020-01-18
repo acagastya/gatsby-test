043803d233046ad027ce109971b7b0baf5369633
Agastya Chandrakant <me@hanabi.in>
2020-01-18 13:32:31 +0530

    Update 404 and about

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,11 +9,19 @@ function About() {
       <article class="entry" lang="en">
         <div className="entry-content">
           <p>
-            Hi, I’m Agastya. Netizens know me as <em>acagastya</em>.
+            Hi, I’m Agastya. I go by the name <em>acagastya</em> across the
+            internet.
           </p>
           <p>
-            I am an undergrad student, and I aim to write about various
-            mathematical and computational topics here.
+            I’m an undergrad student, and I aim to write about various
+            mathematical and computational topics here. But I would also be
+            writing about learning, even if it is not primarily focused on these
+            two topics.
+          </p>
+          <p>
+            If a situation were to arise where you wish to write to me, links to
+            some of my internet accounts are mentioned below. Mention me in a
+            tweet, write an email, or see my pictures on Instagram!
           </p>
         </div>
       </article>
